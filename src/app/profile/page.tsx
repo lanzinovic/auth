61b0eeab7d79ef6239ba0604c67c1cd130bc3ee3
starboy <vkiplangat553@gmail.com>
2024-02-21 08:44:11 +0300

    logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,34 @@
+"use client";
+import axios from "axios";
 import React from "react";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-4 py-2 px-4 rounded"
+      >
+        Logout
+      </button>
     </div>
   );
 }
